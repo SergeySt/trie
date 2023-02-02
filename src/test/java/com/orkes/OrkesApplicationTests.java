076092020dcc8628a13c01f7cc415207d6fbfad7
sergei <sergei@pop-os.localdomain>
2023-02-01 20:36:04 -0700

    init commit

---
+++ src/test/java/com/orkes/OrkesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orkes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrkesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
